3d68dadb7bfd37fd9313ff6b1643465201269e98
Xue Zhang <xz3173@cumc.columbia.edu>
2023-12-02 23:17:54 -0500

    create a basic shiny map

--- shiny/ufo/app.R
+++ shiny/ufo/app.R
@@ -8,42 +8,27 @@
 #
 
 library(shiny)
+library(leaflet)
+library(tidyverse)
+
+ufo_subset = read_csv("./data/ufo_subset.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+    titlePanel("UFO Sightings Map"),
+    leafletOutput("map")
 )
 
+    
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$map <- renderLeaflet({
+      leaflet(ufo_subset) |>
+        addTiles() |>
+        addMarkers(~city_longitude, ~city_latitude, popup = ~as.character(city))
     })
 }
 
